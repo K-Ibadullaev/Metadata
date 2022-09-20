f022b0447226fb5607311868df56858688bc5408
Konstantin <kostantin.ibadullaev.post@gmail.com>
2022-09-20 12:20:18 +0200

    2nd day

--- task0.R
+++ task0.R
@@ -3,7 +3,7 @@ library(magrittr)
 file  = read.csv2("trainingObject.csv", sep = ",")
 df = as.data.frame(file) 
 df %>% head(10)
-
+# df$scr%<>% factor()
 #summary stats
 summary(df)
 
@@ -11,3 +11,14 @@ summary(df)
 fig = plotly:: plot_ly(df,type='scatter3d', x = ~ax, y = ~ay, z = ~az, color = ~scr)
 fig %>% add_markers()
 fig
+
+scr0 = df %>% dplyr::filter(scr==0)
+f0 = plotly:: plot_ly(scr0,type='scatter3d', x = ~ax, y = ~ay, z = ~az, color = ~scr)
+f0 %>% add_markers()
+f0
+
+scr1 = df %>% dplyr::filter(scr==1)
+f1 = plotly:: plot_ly(scr0,type='scatter3d', x = ~ax, y = ~ay, z = ~az, color = ~scr)
+f1 %>% add_markers()
+f1
+
